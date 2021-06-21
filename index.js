1b424cca648c763b198ff2cc27ba90783bcdf514
Viktor1993js <veter4114@gmail.com>
2021-06-21 10:42:39 +0300

    add slider

--- index.js
+++ index.js
@@ -1,56 +1,81 @@
-function Tooltip () {
-    this.tooltip = document.createElement("div");
-    this.tooltip.style.visibility = "hidden";
-    this.tooltip.style.position = "absolute";
-    this.tooltip.className = "mytooltip";
-    this.tooltip.style.zIndex = 1000;
-}
-
-Tooltip.prototype.show = function (text, x, y) {
-    this.tooltip.innerHTML = text;
-    this.tooltip.style.visibility = "visible";
-    this.tooltip.style.left = x + "px";
-    this.tooltip.style.top = y + "px";
-
-    if (this.tooltip.parentNode != document.body) {
-        document.body.appendChild(this.tooltip);
+function tooltip () {
+    function Tooltip () {
+        this.tooltip = document.createElement("div");
+        this.tooltip.style.visibility = "hidden";
+        this.tooltip.style.position = "absolute";
+        this.tooltip.className = "mytooltip";
+        this.tooltip.style.zIndex = 1000;
+    }
+    
+    Tooltip.prototype.show = function (text, x, y) {
+        this.tooltip.innerHTML = text;
+        this.tooltip.style.visibility = "visible";
+        this.tooltip.style.left = x + "px";
+        this.tooltip.style.top = y + "px";
+    
+        if (this.tooltip.parentNode != document.body) {
+            document.body.appendChild(this.tooltip);
+        }
+    }
+    
+    Tooltip.prototype.hide = function () {
+        this.tooltip.style.visibility = "hidden";
+    }
+    
+    window.addEventListener("load", function (){
+        let p = document.getElementsByTagName("p");
+        let button = document.getElementsByTagName("button");
+        let a = document.getElementsByTagName("a");
+        for (let i = 0; i < p.length; i++) {
+            p[i].addEventListener("mousemove", moveHandler);
+            p[i].addEventListener("mouseout", outHandler);
+        }
+        for (let i = 0; i < button.length; i++) {
+            button[i].addEventListener("mousemove", moveHandlerButton);
+            button[i].addEventListener("mouseout", outHandler);
+        }
+        for (let i = 0; i < a.length; i++) {
+            a[i].addEventListener("mousemove", moveHandlerButton);
+            a[i].addEventListener("mouseout", outHandler);
+        }
+    })
+    
+    let tooltip = new Tooltip();
+    
+    function moveHandler (e) {
+        if (!e) e = window.event;
+        tooltip.show("This is text", e.clientX + 15, e.pageY + 15);
+    }
+    
+    function outHandler () {
+        tooltip.hide();
+    }
+    
+    function moveHandlerButton (e) {
+        if (!e) e = window.event;
+        tooltip.show("Click me", e.clientX + 15, e.pageY + 15);
     }
 }
 
-Tooltip.prototype.hide = function () {
-    this.tooltip.style.visibility = "hidden";
-}
+tooltip();
 
-window.addEventListener("load", function (){
-    let p = document.getElementsByTagName("p");
-    let button = document.getElementsByTagName("button");
-    let a = document.getElementsByTagName("a");
-    for (let i = 0; i < p.length; i++) {
-        p[i].addEventListener("mousemove", moveHandler);
-        p[i].addEventListener("mouseout", outHandler);
+function slidesPlugin () {
+    const slides = document.querySelectorAll('.slider');
+    // slides[activeSlide].classList.add('active');
+    
+    for (const slider of slides) {
+        slider.addEventListener('click', () => {
+            clearActiveClasses();
+            slider.classList.add('active');
+        })
     }
-    for (let i = 0; i < button.length; i++) {
-        button[i].addEventListener("mousemove", moveHandlerButton);
-        button[i].addEventListener("mouseout", outHandler);
+    
+    function clearActiveClasses() {
+        slides.forEach((slider) => {
+            slider.classList.remove('active');
+        })
     }
-    for (let i = 0; i < a.length; i++) {
-        a[i].addEventListener("mousemove", moveHandlerButton);
-        a[i].addEventListener("mouseout", outHandler);
-    }
-})
-
-let tooltip = new Tooltip();
-
-function moveHandler (e) {
-    if (!e) e = window.event;
-    tooltip.show("This is text", e.clientX + 15, e.pageY + 15);
 }
 
-function outHandler () {
-    tooltip.hide();
-}
+slidesPlugin();
 
-function moveHandlerButton (e) {
-    if (!e) e = window.event;
-    tooltip.show("Click me", e.clientX + 15, e.pageY + 15);
-}
